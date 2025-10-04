f3cb49fe86e33e9acd4e2745a93251cb5352b80e
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-10-04 06:00:05 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,21 +1,60 @@
-include $(BOARD_DIR)/firmware/firmware.mk
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
+ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+  IS_STM32F429 = yes
+endif
 
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# see also openblt/board.mk STATUS_LED
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::E3
 
-# defines SHORT_BOARD_NAME
-include $(BOARD_DIR)/meta-info.env
+DDEFS += -DEFI_HD_ACR=TRUE
+DDEFS += -DEFI_MAX_31855=TRUE
+DDEFS += -DSTM32_SPI_USE_SPI5=TRUE
 
-# this would save some flash while being unable to update WBO controller firmware
-DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
+DDEFS += -DBOOT_COM_RS232_RX_MAX_DATA=120
+DDEFS += -DOPEN_BLT_TEST_COMMAND=1
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# let's start asap
+DDEFS += -DBOOT_BACKDOOR_ENTRY_TIMEOUT_MS=0
 
-# EGT chip
-#un-comment to enable
-#DDEFS += -DEFI_MAX_31855=TRUE
+# This stuff doesn't work on H7 yet
+ifneq ($(PROJECT_CPU),ARCH_STM32H7)
+	DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+	DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+endif
 
-#see main repo for details on this any many other optional subsystems. We have too many, one has to choose what fits into his choice of stm32
-#DDEFS += -DEFI_ONBOARD_MEMS=TRUE
+# serial ports only on F4
+ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+	# Hardware serial port on UART 2 -> PD5/PD6
+	DDEFS += -DSTM32_UART_USE_USART2=TRUE
+	DDEFS += -DTS_PRIMARY_UxART_PORT=UARTD2
+	DDEFS += -DEFI_CONSOLE_TX_BRAIN_PIN=Gpio::D5 -DEFI_CONSOLE_RX_BRAIN_PIN=Gpio::D6
+endif
+
+# CAND1
+DDEFS += -DBOOT_COM_CAN_CHANNEL_INDEX=0
+DDEFS += -DOPENBLT_CAN_RX_PORT=GPIOD
+DDEFS += -DOPENBLT_CAN_RX_PIN=0
+DDEFS += -DOPENBLT_CAN_TX_PORT=GPIOD
+DDEFS += -DOPENBLT_CAN_TX_PIN=1
+
+ifeq ($(PROJECT_CPU),ARCH_STM32F7)
+  # todo: KNOCK_SPECTROGRAM to use big_buffer
+	DDEFS += -DKNOCK_SPECTROGRAM=TRUE
+	DDEFS += -DLUA_RX_MAX_FILTER_COUNT=96
+
+  # technical debt: UI uses separate ts_show_tcu flag
+  DDEFS += -DEFI_TCU=TRUE
+
+	ifeq ($(EFI_LUA_LOOKUP),)
+    EFI_LUA_LOOKUP = TRUE
+  endif
+  DDEFS += -DEFI_LUA_LOOKUP=$(EFI_LUA_LOOKUP)
+
+	# note #define EFI_EMBED_INI_MSD FALSE in F7 features
+	ifeq ($(DEBUG_LEVEL_OPT),)
+		DEBUG_LEVEL_OPT = -Os -ggdb -g
+	endif
+$(error Unsupported PROJECT_CPU [$(PROJECT_CPU)])
+endif
