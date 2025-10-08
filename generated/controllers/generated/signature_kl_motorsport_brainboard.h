ee79529f5dec6b5a7795faea37c81c69f2786680
GitHub git update Action <action@github.com>
2025-10-08 01:18:41 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_kl_motorsport_brainboard.h
+++ generated/controllers/generated/signature_kl_motorsport_brainboard.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 4285165897
-#define TS_SIGNATURE "rusEFI main.2025.10.07.kl_motorsport_brainboard.4285165897"
+#define TS_SIGNATURE "rusEFI main.2025.10.08.kl_motorsport_brainboard.4285165897"
