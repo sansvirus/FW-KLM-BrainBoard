9be376e89d8b803e297f53775a7fa6ae78e5ac2b
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-10-30 12:46:40 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,6 +1,7 @@
 # List of all the board related files.
 BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
-
+BOARDINC += $(BOARD_DIR)/firmware/controllers/sensors/bmw_mfl.cpp
+   
 # see also openblt/board.mk STATUS_LED
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::E3
 DDEFS += -DEFI_HD_ACR=TRUE
