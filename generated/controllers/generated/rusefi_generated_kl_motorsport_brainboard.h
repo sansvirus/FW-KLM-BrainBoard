6c1ca69e93a96e201d1abfb20ddc6b5612063e84
GitHub git update Action <action@github.com>
2025-11-16 01:28:00 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_kl_motorsport_brainboard.h
+++ generated/controllers/generated/rusefi_generated_kl_motorsport_brainboard.h
@@ -2128,7 +2128,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.11.15.kl_motorsport_brainboard.2317264860"
+#define TS_SIGNATURE "rusEFI main.2025.11.16.kl_motorsport_brainboard.2317264860"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
