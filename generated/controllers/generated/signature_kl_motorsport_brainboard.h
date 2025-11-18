beb980c148c591b553ac2305d05ce8b3d48933dd
GitHub git update Action <action@github.com>
2025-11-18 01:23:07 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_kl_motorsport_brainboard.h
+++ generated/controllers/generated/signature_kl_motorsport_brainboard.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2317264860
-#define TS_SIGNATURE "rusEFI main.2025.11.17.kl_motorsport_brainboard.2317264860"
+#define SIGNATURE_HASH 1930571254
+#define TS_SIGNATURE "rusEFI main.2025.11.18.kl_motorsport_brainboard.1930571254"
